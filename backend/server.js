5e2d8aff0c5b583661058b9234310077c5d3586a
hdcongo61-sudo <hdcongo61@gmail.com>
2026-02-10 17:39:16 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -80,7 +80,13 @@ const corsOptions = {
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'Cache-Control',
+    'Pragma',
+    'Accept'
+  ],
   optionsSuccessStatus: 200
 };
 
