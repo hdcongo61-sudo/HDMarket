c643123483679e844dfb846be9d4a4d99b4e55e0
hdcongo61-sudo <hdcongo61@gmail.com>
2026-06-05 07:45:27 +0100

    Change made

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,6 +3,7 @@ import rateLimit from 'express-rate-limit';
 import { protect } from '../middlewares/authMiddleware.js';
 import { requireRole } from '../middlewares/roleMiddleware.js';
 import { getBundleSuggestions } from '../services/bundleService.js';
+import { upload } from '../utils/upload.js';
 import { validate, schemas } from '../middlewares/validate.js';
 import { cacheMiddleware } from '../utils/cache.js';
 import { idempotencyMiddleware } from '../middlewares/idempotencyMiddleware.js';
--- frontend/dist/assets/useQuery-CX6swE9h.js
+++
@@ -1,1 +0,0 @@
-import{u as r,Q as u}from"./useBaseQuery-CfWM160i.js";function a(e,s){return r(e,u)}export{a as u};
