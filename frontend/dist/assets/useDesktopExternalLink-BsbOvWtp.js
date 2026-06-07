04fdac965d79f64e9940696c06db08455f9a477b
hdcongo61-sudo <hdcongo61@gmail.com>
2026-06-07 23:48:35 +0100

    admin

---
+++ frontend/dist/assets/useDesktopExternalLink-BsbOvWtp.js
@@ -0,0 +1,1 @@
+import{u as e}from"./useIsMobile-3MG8_0bE.js";function n(){return e()?{}:{target:"_blank",rel:"noopener noreferrer"}}export{n as u};
--- frontend/dist/assets/useQuery-BZ6i5ifU.js
+++
@@ -1,1 +0,0 @@
-import{u as r,Q as u}from"./useBaseQuery-Csg6r7Bt.js";function a(e,s){return r(e,u)}export{a as u};
